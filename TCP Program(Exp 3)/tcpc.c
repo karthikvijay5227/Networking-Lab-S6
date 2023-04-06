f5fdd4af01508aebfa0f5ab97fc4514957d9cfe6
Karthik Vijay <kv52274@gmail.com>
2023-04-06 22:01:30 +0530

    Update tcpc.c

--- TCP Program(Exp 3)/tcpc.c
+++ TCP Program(Exp 3)/tcpc.c
@@ -3,16 +3,15 @@
 #include<stdio.h>
 #include<string.h>
 #include<stdlib.h>
-#include <fcntl.h> 
 #include <unistd.h>
 int main()
 
 {
 	char buf[100];
 	int k;
-
+	socklen_t len;
 	int sock_desc;
-	struct sockaddr_in client;
+	struct sockaddr_in server,client;
 	sock_desc=socket(AF_INET, SOCK_STREAM,0);
 
 	if(sock_desc==-1)
@@ -26,13 +25,20 @@ int main()
 
 	if(k==-1)
 		printf("Error in connecting to server\n");
-
-	printf("\nEnter data to be send:");
-	fgets(buf,100,stdin);
-	k=send(sock_desc,buf,100,0);
-	if(k==-1)
-		printf("Error in sending\n");
+	for(;;)
+	{
+		printf("\n[To Server]:");
+		fgets(buf,100,stdin);
+		if(strcmp(buf,"Exit")==0 || strcmp(buf,"exit")==0)
+			exit(0);
+		k=send(sock_desc,buf,100,0);
+		if(k==-1)
+			printf("Error in sending\n");
+		k=recv(sock_desc,buf,100,0);
+		if(k==-1)
+			printf("Error in receiving\n");
+		printf("[From Server]:%s",buf);
+	}
 	close(sock_desc);
 	return 0;
 }
-	
