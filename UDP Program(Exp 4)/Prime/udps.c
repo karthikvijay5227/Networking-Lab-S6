4083e8e736062ff6a4a58a55eeb21a1ed0573227
Karthik Vijay <kv52274@gmail.com>
2023-04-11 15:45:38 +0530

    Update udps.c

--- UDP Program(Exp 4)/Prime/udps.c
+++ UDP Program(Exp 4)/Prime/udps.c
@@ -3,14 +3,26 @@
 #include<sys/socket.h>
 #include<stdlib.h>
 #include<netdb.h>
+#include <stdbool.h>
+#include <math.h>
 
+bool is_prime(int n) {
+
+    if (n <= 1) return false;
+    if (n <= 3) return true;
+    if (n % 2 == 0 || n % 3 == 0) return false;
+    for (int i = 5; i * i <= n; i = i + 6)
+        if (n % i == 0 || n % (i + 2) == 0)
+            return false;
+    return true;
+}
 int main(int argc,char *argv[])
 {
 	struct sockaddr_in server,client;
 	if(argc!=2)
 		printf("Input format not correct");
 	int sockfd = socket(AF_INET,SOCK_DGRAM,0);
-	int f=0;
+	int f=1,i=0;
 	if(sockfd==-1)
 		printf("Error in socket()");
 	server.sin_family=AF_INET;
@@ -25,32 +37,18 @@ int main(int argc,char *argv[])
 		int k = recvfrom(sockfd,buffer,100,0,(struct sockaddr *) &server,&server_len);
 		if(k<0)
 			printf("Error in recvfrom()");
-		if(strncmp("exit",buffer,4)==0||strncmp("Exit",buffer,4)==0)
+		buffer[100] = '\0';
+		if(strncmp("exit",buffer,4)==0 || strncmp("Exit",buffer,4)==0)
 		{
-			printf("Received exit request from client!\nExiting!!!");
+			printf("Received exit request from client!\nExiting!!!\n");
 			break;
 		}
-		else
-		{
-			printf("\nMessage from Client:%s",buffer);
-			int c = atoi(buffer);
-			if (c == 0 || c == 1)
-    			f = 1;
-			for(int i=2;i<=c/2;i++)
-			{
-				if(c%i==0){
-					f=1;
-					break;
-				}	
-			}
-			if(f==0)
-				strcpy(buffer,"The number is prime");
-			strcpy(buffer,"The number is not prime");
-			
-			k = sendto(sockfd,buffer,sizeof(buffer),0,(struct sockaddr*) &server,sizeof(server));
-			if(k<0)
-				printf("Error in sendto()");
-		}
+		int n = atoi(buffer);
+		bool isPrime = is_prime(n);
+		char *res = isPrime ? "Number is Prime" : "Number is Not Prime";
+		k = sendto(sockfd,res,sizeof(res),0,(struct sockaddr*) &server,sizeof(server));
+		if(k<0)
+			printf("Error in sendto()");
 	}
 	return 0;
 }
