7804142d0463261035f3d330e2cd452b961ff49e
Karthik Vijay <kv52274@gmail.com>
2023-04-14 20:02:05 +0530

    Add files via upload

---
+++ TCP Program(Exp 3)/With read,write functions/Factorial/tcpsf.c
@@ -0,0 +1,67 @@
+#include<sys/socket.h>
+#include<netinet/in.h>
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+int main()
+{
+	char buf[100],bufs[100];
+	int k;
+	socklen_t len;
+	int sock_desc,temp_sock_desc;
+	struct sockaddr_in server,client;
+	
+	sock_desc=socket(AF_INET,SOCK_STREAM,0);
+	
+	if(sock_desc==-1)
+		printf("Error in Socket creation\n");
+
+	server.sin_family=AF_INET;
+	server.sin_addr.s_addr=INADDR_ANY;
+
+	server.sin_port=3003;
+	client.sin_family=AF_INET;
+	
+	client.sin_addr.s_addr=INADDR_ANY;
+	client.sin_port=3003;
+	
+	k=bind(sock_desc,(struct sockaddr*) &server,sizeof(server));
+	
+	if(k==-1)
+		printf("Error in binding\n");
+		
+	k=listen(sock_desc,5);
+	
+	if(k==-1)
+		printf("Error in listening\n");
+		
+	len=sizeof(client);
+	temp_sock_desc=accept(sock_desc,(struct sockaddr*) &client,&len);
+		
+	if(temp_sock_desc==-1)
+		printf("Error in temporary socket creation\n");
+	for(;;)
+	{
+	        bzero(buf,sizeof(buf));
+		read(temp_sock_desc,buf,sizeof(buf));
+			
+		int c = atoi(buf);
+		if(strncmp("exit",buf,4)==0 || strncmp("Exit",buf,4)==0)
+		{
+			printf("\nGot Exit Request from Client!\nExiting!!!\n");
+			exit(0);
+		}
+		printf("\nMessage got from client is:%s",buf);
+		int fact=1;
+		for(int i=1;i<=c;i++)
+			fact*=i;
+		sprintf(bufs,"%d", fact);
+		write(temp_sock_desc,bufs,sizeof(bufs));
+	}
+	return 0;
+}
+	
+	
+	
