e0b4547de89cd5c7f60d6201eb888f8018a1903a
Karthik Vijay <kv52274@gmail.com>
2023-06-21 20:15:07 +0530

    Update tcps.c

--- TCP Program(Exp 3)/With read,write functions/tcps.c
+++ TCP Program(Exp 3)/With read,write functions/tcps.c
@@ -1,8 +1,8 @@
-#include<sys/socket.h>
-#include<netinet/in.h>
-#include<stdio.h>
-#include<string.h>
-#include<stdlib.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 #include <fcntl.h>
 #include <unistd.h>
 int main()
