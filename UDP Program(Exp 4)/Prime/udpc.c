f0d40ff73a5243ad809cb4846c69510d6957ca14
Karthik Vijay <kv52274@gmail.com>
2023-08-08 21:59:10 +0530

    Update udpc.c

--- UDP Program(Exp 4)/Prime/udpc.c
+++ UDP Program(Exp 4)/Prime/udpc.c
@@ -4,40 +4,40 @@
 #include<stdlib.h>
 #include<netdb.h>
 
-int main(int argc,char *argv[])
+int main()
 {
-	struct sockaddr_in server,client;
-	if(argc!=3)
-		printf("Input format not correct\n");
-	int sockfd = socket(AF_INET,SOCK_DGRAM,0);
+	char buffer[100];	
 	int k;
-	if(sockfd==-1)
+	struct sockaddr_in server,client;
+	int sock_desc = socket(AF_INET,SOCK_DGRAM,0);
+	if(sock_desc == -1)
 		printf("Error in socket()\n");
+		
 	server.sin_family=AF_INET;
 	server.sin_addr.s_addr=INADDR_ANY;
-	server.sin_port = htons(atoi(argv[2]));
-	socklen_t server_len = sizeof(server);
+	server.sin_port = 2060;
+
 	socklen_t client_len = sizeof(client);
-	char buffer[100];
+
 	for(;;)
 	{
 		printf("\nEnter the number to check prime:");
 		fgets(buffer,100,stdin);
 		if(strncmp("exit",buffer,4)==0 || strncmp("Exit",buffer,4)==0)
 		{
-			k = sendto(sockfd,buffer,sizeof(buffer),0,(struct sockaddr*) &server,sizeof(server));
-			if(k<0)
+			k = sendto(sock_desc,buffer,sizeof(buffer),0,(struct sockaddr*) &server,sizeof(server));
+			if(k == -1)
 				printf("Error in exit request");
 			printf("Exit Request Sent!\nExiting!!!\n");
 			break;
 		}
 		else
 		{
-			k = sendto(sockfd,buffer,sizeof(buffer),0,(struct sockaddr*) &server,sizeof(server));
-			if(k<0)
+			k = sendto(sock_desc,buffer,sizeof(buffer),0,(struct sockaddr*) &server,sizeof(server));
+			if(k == -1)
 				printf("\nError in sendto()");
-			k = recvfrom(sockfd,buffer,100,0,(struct sockaddr *) &client,&client_len);
-			if(k<0)
+			k = recvfrom(sock_desc,buffer,100,0,(struct sockaddr *) &client,&client_len);
+			if(k == -1)
 				printf("\nError in recvfrom()");
 			printf("%s\n",buffer);
 		}
