ce6155ab54542d92bb103bd7d57d9a030b90a4a2
Karthik Vijay <kv52274@gmail.com>
2023-08-08 22:06:40 +0530

    Update udps.c

--- UDP Time Server/New/udps.c
+++ UDP Time Server/New/udps.c
@@ -6,27 +6,28 @@
 #include <arpa/inet.h>
 #include <time.h>
 
-int main(int argc, char *argv[])
+int main()
 {
 	int num;
 	struct sockaddr_in server, client;
-	if (argc != 2)
-		printf("Input format not correct");
 	int sockfd = socket(AF_INET, SOCK_DGRAM, 0);
 	if (sockfd == -1)
-		printf("Error in socket()");
+		printf("Error in socket creation");
+		
 	server.sin_family = AF_INET;
 	server.sin_addr.s_addr = inet_addr("127.0.0.1");
-	server.sin_port = htons(atoi(argv[1]));
+	server.sin_port = 2060;
+	
 	if (bind(sockfd, (struct sockaddr *)&server, sizeof(server)) < 0)
-		printf("Error in bind()\n");
+		printf("Error in binding\n");
+		
 	socklen_t server_len = sizeof(server);
 	printf("Server is running on %s\n",inet_ntoa(server.sin_addr));
 	while(1)
 	{
 		recvfrom(sockfd, &num,sizeof(num), 0, (struct sockaddr *)&server, &server_len);
 		time_t current_time = time(NULL);
-		printf("Client asked for time:%s\n",ctime(&current_time));
+		printf("Client asked for time:%s",ctime(&current_time));
 		sendto(sockfd, &current_time, sizeof(current_time), 0, (struct sockaddr *)&server, sizeof(server));
 		exit(0);
 	}
