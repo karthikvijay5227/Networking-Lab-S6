77d52bf53d8fbba3923a92bf2013f6a8db668f63
Karthik Vijay <kv52274@gmail.com>
2023-08-08 19:42:30 +0530

    Update ftpc.c

--- FTP/New/ftpc.c
+++ FTP/New/ftpc.c
@@ -1,10 +1,10 @@
-#include<stdio.h>
-#include<string.h>
-#include<stdlib.h>
-#include<sys/socket.h>
-#include<netinet/in.h>
-#include<unistd.h>
-#include<fcntl.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <fcntl.h>
 
 void main()
 {
