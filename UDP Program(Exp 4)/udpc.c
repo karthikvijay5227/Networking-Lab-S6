4d0165a5a043aea2b8581df05ab21016d43bf308
Karthik Vijay <kv52274@gmail.com>
2023-08-08 21:49:54 +0530

    Update udpc.c

--- UDP Program(Exp 4)/udpc.c
+++ UDP Program(Exp 4)/udpc.c
@@ -1,31 +1,30 @@
-#include<stdio.h>
-#include<string.h>
-#include<sys/socket.h>
-#include<stdlib.h>
-#include<netdb.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <stdlib.h>
+#include <netdb.h>
 
-int main(int argc,char *argv[])
+int main()
 {
+	char buffer[100];
 	struct sockaddr_in server,client;
-	if(argc!=3)
-		printf("Input format not correct\n");
-	int sockfd = socket(AF_INET,SOCK_DGRAM,0);
+	int sock_desc = socket(AF_INET,SOCK_DGRAM,0);
 	int k;
-	if(sockfd==-1)
-		printf("Error in socket()\n");
+	if(sock_desc==-1)
+		printf("Error in socket creation\n");
+		
 	server.sin_family=AF_INET;
 	server.sin_addr.s_addr=INADDR_ANY;
-	server.sin_port = htons(atoi(argv[2]));
-	socklen_t server_len = sizeof(server);
-	socklen_t client_len = sizeof(client);
-	char buffer[100];
+	server.sin_port = 2060;
+
+	socklen_t len = sizeof(client);
 	for(;;)
 	{
 		printf("\nEnter a message to be sent to server:");
 		fgets(buffer,100,stdin);
 		if(strncmp("exit",buffer,4)==0||strncmp("Exit",buffer,4)==0)
 		{
-			k = sendto(sockfd,buffer,sizeof(buffer),0,(struct sockaddr*) &server,sizeof(server));
+			k = sendto(sock_desc,buffer,sizeof(buffer),0,(struct sockaddr*) &server,sizeof(server));
 			if(k<0)
 				printf("Error in exit request");
 			printf("Exit Request Sent\nExiting");
@@ -33,12 +32,12 @@ int main(int argc,char *argv[])
 		}
 		else
 		{
-			k = sendto(sockfd,buffer,sizeof(buffer),0,(struct sockaddr*) &server,sizeof(server));
+			k = sendto(sock_desc,buffer,sizeof(buffer),0,(struct sockaddr*) &server,sizeof(server));
 			if(k<0)
-				printf("\nError in sendto");
-			k = recvfrom(sockfd,buffer,100,0,(struct sockaddr *) &client,&client_len);
+				printf("\nError in sending");
+			k = recvfrom(sock_desc,buffer,100,0,(struct sockaddr *) &client,&len);
 			if(k<0)
-				printf("\nError in recvfrom()");
+				printf("\nError in receiving");
 			printf("Message from Server:%s\n",buffer);
 		}
 	}
