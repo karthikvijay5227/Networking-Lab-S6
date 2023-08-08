3ad19a474d22fbb4bbc4eb04de0199c53ab72820
Karthik Vijay <kv52274@gmail.com>
2023-08-08 21:49:29 +0530

    Update udps.c

--- UDP Program(Exp 4)/udps.c
+++ UDP Program(Exp 4)/udps.c
@@ -1,29 +1,31 @@
-#include<stdio.h>
-#include<string.h>
-#include<sys/socket.h>
-#include<stdlib.h>
-#include<netdb.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <stdlib.h>
+#include <netdb.h>
 
-int main(int argc,char *argv[])
+int main()
 {
+	char buffer[100];
+	int k;
 	struct sockaddr_in server,client;
-	if(argc!=2)
-		printf("Input format not correct");
-	int sockfd = socket(AF_INET,SOCK_DGRAM,0);
-	if(sockfd==-1)
-		printf("Error in socket()");
+	int sock_desc = socket(AF_INET,SOCK_DGRAM,0);
+	if(sock_desc==-1)
+		printf("Error in socket creation");
+		
 	server.sin_family=AF_INET;
 	server.sin_addr.s_addr=INADDR_ANY;
-	server.sin_port = htons(atoi(argv[1]));
-	if(bind(sockfd,(struct sockaddr*) &server,sizeof(server))<0)
-		printf("Error in bind()\n");
-	char buffer[100];
-	socklen_t server_len = sizeof(server);
+	server.sin_port = 2060;
+	
+	if(bind(sock_desc,(struct sockaddr*)&server,sizeof(server))<0)
+		printf("Error in binding\n");
+
+	socklen_t len = sizeof(server);
 	for(;;)
 	{
-		int k = recvfrom(sockfd,buffer,100,0,(struct sockaddr *) &server,&server_len);
+		k = recvfrom(sock_desc,buffer,100,0,(struct sockaddr *) &server,&len);
 		if(k<0)
-			printf("Error in recvfrom()");
+			printf("Error in receiving");
 		if(strncmp("exit",buffer,4)==0||strncmp("Exit",buffer,4)==0)
 		{
 			printf("Received exit request from client!\nExiting!!!");
@@ -35,9 +37,9 @@ int main(int argc,char *argv[])
 			printf("Enter data to send to client:");
 			fgets(buffer,100,stdin);
 			printf("\n");
-			k = sendto(sockfd,buffer,sizeof(buffer),0,(struct sockaddr*) &server,sizeof(server));
+			k = sendto(sock_desc,buffer,sizeof(buffer),0,(struct sockaddr*) &server,sizeof(server));
 			if(k<0)
-				printf("Error in sendto()");
+				printf("Error in sending");
 		}
 	}
 	return 0;
